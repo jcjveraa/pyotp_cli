150427a189d4c9ba651a2fb537651b4b389a9e94
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2018-08-07 16:47:43 +0200

    Uploading setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+install_requires = ['pyotp']
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyotp_cli",
+    version="0.0.3",
+    url="https://github.com/jcjveraa/pyotp_cli",
+    license="MIT License",
+    author="Jelle Veraa",
+    author_email="3942301+jcjveraa@users.noreply.github.com",
+    description="A small example package",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    test_suite="test",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
